f225a775eed6f31824d34e897369ff9b7ac9d88e
Caden Christesen <BaptistChristesen@gmail.com>
2022-10-27 14:11:02 -0500

    added alert (has errors); added check for winner function

--- TicTacToe/ContentView.swift
+++ TicTacToe/ContentView.swift
@@ -10,6 +10,8 @@ import SwiftUI
 struct ContentView: View {
     @State private var moves = Array(repeating: "", count: 9)
     @State private var xTurn = true
+    @State private var gameOver = false
+    @State private var winMessage = ""
     var body: some View {
         VStack {
             Text("Tic-Tac-Toe")
@@ -40,11 +42,24 @@ struct ContentView: View {
             }
         }
         .preferredColorScheme(.dark)
+        .alert(isPresented: $gameOver){
+            Alert(title: Text(winMessage))
+         }
+         .onChange(of: moves) {
+             checkForWinner()
+        }
+        
     }
     
     struct ContentView_Previews: PreviewProvider {
         static var previews: some View {
             ContentView()
+        }
+    }
+    private func checkForWinner(){
+        if moves[0] != "" && moves[0] == moves[1] && moves[1] == moves[2]{
+            winMessage = "\(moves [0]) is the winner!"
+            gameOver = true
         }
     }
 }
